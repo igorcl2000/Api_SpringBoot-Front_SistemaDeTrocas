f06e73f24cde06e8e4830da11418fdb805121260
igorcl2000 <igorcl2000@gmail.com>
2023-05-24 16:05:12 -0300

    Listagem Medicos
    Listagem Medicos 001

--- api/src/main/java/med/voli/api/endereco/DadosEndereco.java
+++
@@ -1,6 +0,0 @@
-package med.voli.api.endereco;
-
-public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento,
-                String numero) {
-
-}
--- api/src/main/java/med/voli/api/medico/DadosCadastroMedico.java
+++
@@ -1,7 +0,0 @@
-package med.voli.api.medico;
-
-import med.volli.api.endereco.DadosEndereco;
-
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade,
-                DadosEndereco endereco) {
-}
